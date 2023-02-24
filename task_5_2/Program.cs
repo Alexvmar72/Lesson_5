893256361d518a70c60e6aa6dee1fcfc6a27da5a
Alex Mark <alexvmar@yandex.ru>
2023-02-24 17:54:10 +0300

    change file2 homework5

--- task_5_2/Program.cs
+++ task_5_2/Program.cs
@@ -6,3 +6,38 @@
 
 [3, 7, 23, 12] -> 19
 [-4, -6, 89, 6] -> 0                 */
+
+Console.Clear();
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int num_result = int.Parse(Console.ReadLine()!);
+    return num_result;
+}
+
+int[] GetBinaryArray(int len, int min_pos, int max_pos)
+{
+    int[] arr_result = new int[len];
+    for (int j = 0; j < len; j++)
+    {
+        arr_result[j] = new Random().Next(min_pos, max_pos + 1);
+    }
+    return arr_result;
+}
+
+int SumOdd(int[] array)
+{
+    int len_2 = array.Length;
+    int sum = 0;
+    for (int i = 1; i < len_2; i = i + 2) sum = sum + array[i];
+    return sum;
+}
+
+int len = Prompt("Введите размер массива: ");
+int min_pos = Prompt("Введите минимальное значение массива: ");
+int max_pos = Prompt("Введите максимальное значение массива: ");
+int[] table = GetBinaryArray(len, min_pos, max_pos);
+int sum_odd = SumOdd(table);
+
+Console.WriteLine($"[{string.Join(", ", table)}] -> {sum_odd}");
